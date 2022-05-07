c9318f8c071e3efe05dcd57c6cde2db0e0263af9
weberth <weberthmo@gmail.com>
2022-05-07 02:57:31 -0300

    finalizando

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -2,7 +2,8 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post, Res } from '@nestjs/common';
+import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 
 @Controller('users') // define classe como controladora
@@ -12,10 +13,40 @@ export class UsuarioController {
     // private usuariosService = new UsuarioService(); //instanciada
     constructor(private usuarioService: UsuarioService){} //injetada
 
+    
+    @Get(':nomeDeUsuario')
+    public buscaPorNomeDeUsuario(@Param('nomeDeUsuario') nomeDeUsuario: string){
+        const usuarioEncontrado = this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario)
+        if (!usuarioEncontrado) {
+            // throw new NotFoundException("Não encontrado"); //codigo de erro
+            throw new NotFoundException(
+                {
+                    statusCode: HttpStatus.NOT_FOUND,
+                    message: "Usuário não encontrado"
+                }
+            );
+        }
+        return usuarioEncontrado
+    }
+
     //metodo
+    //resposta em NestJs
     @Post()
-    public cria(@Body() usuario){
+    public cria(@Body() usuario: Usuario): Usuario {
         const usuarioCriado = this.usuarioService.cria(usuario)
         return usuarioCriado;
     }
+
+    //resposta com express
+    //implementa localização da criação do cadastro, nivel REST
+    // @Post()
+    // public cria(@Body() usuario: Usuario, @Res() res) {
+    //     const usuarioCriado = this.usuarioService.cria(usuario)
+    //     res.status(HttpStatus.CREATED)
+    //         .location(`/users/${usuarioCriado.nomeDeUsuario}`)
+    //         .json(usuarioCriado);
+    // }
+
+
+    
 }
